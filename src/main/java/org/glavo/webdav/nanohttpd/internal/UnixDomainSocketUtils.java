a107123143fe566949ca7d2ced03361d0a2bcb9b
Glavo <zjx001202@gmail.com>
2023-07-26 17:51:49 +0800

    UnixDomainSocketUtils

---
+++ src/main/java/org/glavo/webdav/nanohttpd/internal/UnixDomainSocketUtils.java
@@ -0,0 +1,80 @@
+package org.glavo.webdav.nanohttpd.internal;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.net.ProtocolFamily;
+import java.net.SocketAddress;
+import java.net.StandardProtocolFamily;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.file.Path;
+
+public final class UnixDomainSocketUtils {
+    private static final boolean AVAILABLE;
+
+    private static final MethodHandle createUnixDomainSocketAddress;
+    private static final MethodHandle openUnixDomainServerSocketChannel;
+
+
+    static {
+        boolean available = false;
+
+        StandardProtocolFamily unixProtocolFamily;
+
+        MethodHandle createUnixDomainSocketAddressHandle = null;
+        MethodHandle openUnixDomainServerSocketChannelHandle = null;
+
+        try {
+            unixProtocolFamily = StandardProtocolFamily.valueOf("UNIX");
+
+            MethodHandles.Lookup lookup = MethodHandles.publicLookup();
+
+            Class<?> udsaClass = Class.forName("java.net.UnixDomainSocketAddress");
+            createUnixDomainSocketAddressHandle = lookup.findStatic(udsaClass, "of",
+                            MethodType.methodType(udsaClass, Path.class))
+                    .asType(MethodType.methodType(SocketAddress.class, Path.class));
+
+            openUnixDomainServerSocketChannelHandle = lookup.findStatic(ServerSocketChannel.class, "open",
+                            MethodType.methodType(ServerSocketChannel.class, ProtocolFamily.class))
+                    .bindTo(unixProtocolFamily);
+
+            available = true;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        AVAILABLE = available;
+        createUnixDomainSocketAddress = createUnixDomainSocketAddressHandle;
+        openUnixDomainServerSocketChannel = openUnixDomainServerSocketChannelHandle;
+    }
+
+    public static void checkAvailable() {
+        if (!AVAILABLE) {
+            throw new UnsupportedOperationException("Please upgrade to Java 16+");
+        }
+    }
+
+    public static SocketAddress createUnixDomainSocketAddress(Path path) {
+        try {
+            return (SocketAddress) createUnixDomainSocketAddress.invokeExact(path);
+        } catch (IllegalArgumentException | NullPointerException e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new InternalError(e);
+        }
+    }
+
+    public static ServerSocketChannel openUnixDomainServerSocketChannel() throws IOException {
+        try {
+            return (ServerSocketChannel) openUnixDomainServerSocketChannel.invokeExact();
+        } catch (IOException | NullPointerException e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new InternalError(e);
+        }
+    }
+
+    private UnixDomainSocketUtils() {
+    }
+}
