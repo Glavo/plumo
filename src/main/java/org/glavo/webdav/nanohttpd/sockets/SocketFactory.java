bdd7d801da7a1d7641475cd1e129c9421a603ba2
Glavo <zjx001202@gmail.com>
2023-07-29 18:07:16 +0800

    Delete org.glavo.webdav.nanohttpd.sockets

--- src/main/java/org/glavo/webdav/nanohttpd/sockets/SocketFactory.java
+++
@@ -1,9 +0,0 @@
-package org.glavo.webdav.nanohttpd.sockets;
-
-import java.io.IOException;
-import java.net.ServerSocket;
-
-@FunctionalInterface
-public interface SocketFactory {
-    ServerSocket create() throws IOException;
-}
