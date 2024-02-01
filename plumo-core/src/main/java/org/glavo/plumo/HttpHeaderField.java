b3cf9b0e4694336320deffe53d8ccdf4d14a82b0
Glavo <zjx001202@gmail.com>
2024-02-02 05:19:29 +0800

    Create HttpHeaderField

---
+++ plumo-core/src/main/java/org/glavo/plumo/HttpHeaderField.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2023 Glavo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.glavo.plumo;
+
+import org.glavo.plumo.internal.util.Utils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public final class HttpHeaderField {
+
+    @SuppressWarnings("deprecation")
+    public static HttpHeaderField of(String value) {
+        int len = value.length(); // implicit null check
+        if (len == 0) {
+            throw new IllegalArgumentException("Header name must not be empty");
+        }
+        if (len > 80) {
+            throw new IllegalArgumentException("Header name is too long");
+        }
+
+        int i = 0;
+        while (i < len) {
+            char ch = value.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                break;
+            } else if (Utils.isTokenPart(ch)) {
+                i++;
+            } else {
+                throw new IllegalArgumentException("Invalid header name: " + value);
+            }
+        }
+
+        byte[] bytes = new byte[len];
+        if (i > 0) {
+            value.getBytes(0, i, bytes, 0);
+        }
+
+        while (i < len) {
+            char ch = value.charAt(i);
+            if (ch >= 'A' && ch <= 'Z') {
+                bytes[i] = (byte) (ch | 0x20); // The oldest trick in the ASCII book
+            } else if (Utils.isTokenPart(ch)) {
+                bytes[i] = (byte) ch;
+            } else {
+                throw new IllegalArgumentException("Invalid header name: " + value);
+            }
+            i++;
+        }
+
+        return new HttpHeaderField(bytes);
+    }
+
+    private final byte[] bytes;
+    private int hash;
+
+    private String string;
+
+    private HttpHeaderField(byte[] bytes) {
+        this.bytes = bytes;
+    }
+
+    public int length() {
+        return bytes.length;
+    }
+
+    @Override
+    public int hashCode() {
+        int h = this.hash;
+        if (h != 0) {
+            return h;
+        }
+
+        h = Arrays.hashCode(bytes);
+        if (h == 0) {
+            h = 644954127; // magic number
+        }
+        return this.hash = h;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof HttpHeaderField && Arrays.equals(this.bytes, ((HttpHeaderField) obj).bytes);
+    }
+
+    @Override
+    public String toString() {
+        if (string == null) {
+            string = new String(bytes, StandardCharsets.US_ASCII);
+        }
+        return string;
+    }
+}
