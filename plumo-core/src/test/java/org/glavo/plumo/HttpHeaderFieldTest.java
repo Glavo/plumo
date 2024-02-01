55da06996a0b1ab9dec036ce1538482238252be6
Glavo <zjx001202@gmail.com>
2024-02-02 05:45:06 +0800

    Create HttpHeaderFieldTest

---
+++ plumo-core/src/test/java/org/glavo/plumo/HttpHeaderFieldTest.java
@@ -0,0 +1,34 @@
+package org.glavo.plumo;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public final class HttpHeaderFieldTest {
+
+    @Test
+    public void testVerification() {
+        assertThrows(IllegalArgumentException.class, () -> HttpHeaderField.of(""));
+        assertThrows(IllegalArgumentException.class, () -> HttpHeaderField.of("content type"));
+        assertThrows(IllegalArgumentException.class, () -> HttpHeaderField.of("测试"));
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("content-type", HttpHeaderField.of("content-type").toString());
+        assertEquals("content-type", HttpHeaderField.of("Content-Type").toString());
+    }
+
+    @Test
+    public void testHash() {
+        HashMap<HttpHeaderField, String> map = new HashMap<>();
+        map.put(HttpHeaderField.of("content-type"), "value1");
+        map.put(HttpHeaderField.of("content-length"), "value2");
+
+
+        assertEquals("value1", map.get(HttpHeaderField.of("Content-Type")));
+        assertEquals("value2", map.get(HttpHeaderField.of("Content-Length")));
+    }
+}
