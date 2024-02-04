64be78633a28d978bbc9e064b9d8fc72c9260f6f
Glavo <zjx001202@gmail.com>
2024-02-05 06:43:06 +0800

    Create InputWrapper

---
+++ plumo-core/src/main/java/org/glavo/plumo/internal/util/InputWrapper.java
@@ -0,0 +1,7 @@
+package org.glavo.plumo.internal.util;
+
+import java.io.InputStream;
+import java.nio.channels.ReadableByteChannel;
+
+public abstract class InputWrapper extends InputStream implements ReadableByteChannel {
+}
