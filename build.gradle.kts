d539f813b3d3505d7e5fbd46293622545be636cc
Glavo <zjx001202@gmail.com>
2024-02-16 23:07:19 +0800

    Bump JUnit to 5.10.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ subprojects {
 
     dependencies {
         compileOnlyApi("org.jetbrains:annotations:24.0.1")
-        testImplementation(platform("org.junit:junit-bom:5.10.1"))
+        testImplementation(platform("org.junit:junit-bom:5.10.2"))
         testImplementation("org.junit.jupiter:junit-jupiter")
     }
 
