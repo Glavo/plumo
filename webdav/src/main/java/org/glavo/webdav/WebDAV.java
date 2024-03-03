912816301b4c9b778ba4abb61a20dd0eabb6d07a
Glavo <zjx001202@gmail.com>
2024-03-03 22:26:53 +0800

    Create WebDAV.getVersion()

--- webdav/src/main/java/org/glavo/webdav/WebDAV.java
+++ webdav/src/main/java/org/glavo/webdav/WebDAV.java
@@ -15,7 +15,23 @@
  */
 package org.glavo.webdav;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public final class WebDAV {
+    private static String getVersion() {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(
+                WebDAV.class.getResourceAsStream("version.txt"), StandardCharsets.UTF_8))) {
+            return reader.readLine();
+        } catch (IOException | NullPointerException e) {
+            return "unknown";
+        }
+    }
+
+
+
     public static void main(String[] args) {
         // TODO
     }
